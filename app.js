fb8d4c8d9af53a43e4100b5abe29712676c10410
Mihir <mihirpatel8329@gmail.com>
2023-02-12 11:49:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+let luckyNum = 12
+
+luckyNum += 8
+
+console.log("Number is "+ luckyNum)
+
+console.log("Type of Lucky num is "+ typeof(luckyNum))
+
+const finalLuckyNum = {
+    // "finalLuckyNum" : luckyNum
+}
+
+finalLuckyNum["finalLuckyNum"] = luckyNum
+
+
+console.log(finalLuckyNum["finalLuckyNum"])
+
+const workMeaning = {
+    name : "Mihir",
+    dateOfBirth : "12-04-2002",
+    name : "Zily",
+}
+
+
+
+console.log(workMeaning.dateOfBirth)
